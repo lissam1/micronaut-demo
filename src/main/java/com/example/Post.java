13ea6b89f36aaf660b094993538ea557cb2d2238
issam liqali <liqaliissam@gmail.com>
2025-08-11 12:52:22 +0100

    micronaut serialization

---
+++ src/main/java/com/example/Post.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import io.micronaut.serde.annotation.Serdeable;
+
+@Serdeable
+public record Post(
+        Long id,
+        Long userId,
+        String title,
+        String body ) {
+}
