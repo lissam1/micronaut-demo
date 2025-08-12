bde1d41d4800297c53d929a770c8172baf94e53a
issam liqali <liqaliissam@gmail.com>
2025-08-12 16:59:43 +0100

    builder annotation

--- src/main/java/com/example/contact/Contact.java
+++ src/main/java/com/example/contact/Contact.java
@@ -1,5 +1,10 @@
 package com.example.contact;
 
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.sourcegen.annotations.Builder;
+
+@Introspected
+@Builder
 public record Contact(
         String firstName,
         String lastName,
