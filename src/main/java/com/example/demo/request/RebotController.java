5cf07c88076a88a2ecf4fc51d6197fef65ac41d4
issam liqali <liqaliissam@gmail.com>
2025-08-22 12:40:39 +0100

    @RequestScope Scope

---
+++ src/main/java/com/example/demo/request/RebotController.java
@@ -0,0 +1,27 @@
+package com.example.demo.request;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller("/request")
+public class RebotController {
+
+    private final RebotFather rebotFather;
+    private final RebotMother rebotMother;
+
+    public RebotController(RebotFather rebotFather, RebotMother rebotMother){
+        this.rebotFather = rebotFather;
+        this.rebotMother = rebotMother;
+    }
+
+    @Get
+    Set<String> children(){
+        Set<String> serilaNumbers = new HashSet<>();
+        serilaNumbers.add(rebotFather.child().getSerialNumber());
+        serilaNumbers.add(rebotMother.child().getSerialNumber());
+        return serilaNumbers;
+    }
+}
