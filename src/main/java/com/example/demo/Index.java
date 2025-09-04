e9eb0ebcfd35267a13a821bb7cd00cdee976b190
issam liqali <liqaliissam@gmail.com>
2025-09-04 12:20:46 +0100

    query values

--- src/main/java/com/example/demo/Index.java
+++ src/main/java/com/example/demo/Index.java
@@ -3,14 +3,29 @@ package com.example.demo;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.http.annotation.QueryValue;
+import jakarta.annotation.Nullable;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @Controller("/demo")
 public class Index {
+
+    private static final Logger log = Logger.getLogger(Index.class.getName());
+    private static final Map<String, Object> INDEX = Collections.singletonMap("message", "Hello World!");
 
     @Get("/{from}/{to}")
     public String exchange(@PathVariable("from") String fromCode,
                            @PathVariable("to") String toCode) {
         return "Exchange from " + fromCode + " to " + toCode;
+    }
+
+    @Get("/source/")
+    Map<String, Object> getIndex(@Nullable @QueryValue("utm_source") String source) {
+        log.info("source: " + source);
+        return INDEX;
     }
 
 }
