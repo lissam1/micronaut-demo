70d66e5dbc48018bb9f7617edc5e015bf6ad47aa
issam liqali <liqaliissam@gmail.com>
2025-09-25 14:28:16 +0100

    Server Filter

---
+++ src/main/java/com/example/cache/CacheControlFilter.java
@@ -0,0 +1,16 @@
+package com.example.cache;
+
+import io.micronaut.http.HttpHeaders;
+import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.http.annotation.ResponseFilter;
+import io.micronaut.http.annotation.ServerFilter;
+
+@ServerFilter("/assets/bootstrap*/**")
+public class CacheControlFilter {
+    @ResponseFilter
+    public void filterResponse(MutableHttpResponse<?> response) {
+        if(!response.getHeaders().contains(HttpHeaders.CACHE_CONTROL)){
+            response.getHeaders().add(HttpHeaders.CACHE_CONTROL, "public, max-age=31536000, imutable");
+        }
+    }
+}
