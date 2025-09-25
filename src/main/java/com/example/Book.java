68a5926ca035536fbe38d750492f856e6c19b48b
issam liqali <liqaliissam@gmail.com>
2025-09-25 16:39:08 +0100

    define validation constraints

---
+++ src/main/java/com/example/Book.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+
+@Introspected
+public record Book(@NotBlank String name, @Nullable String description,@Positive Integer pages) {
+}
